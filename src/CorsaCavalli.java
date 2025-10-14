04b6ec475ce39be4ddce2cb6dd84225dbaf61853
studente <prova@1234>
2025-10-14 09:17:06 +0200

    primo commit

--- src/CorsaCavalli.java
+++ src/CorsaCavalli.java
@@ -1,20 +1,22 @@
 public class CorsaCavalli extends Thread {
     private final int num_cavalli;
     protected String name;
+    protected int lentezza;
 
-    public CorsaCavalli(int num_cavalli, String name){
+    public CorsaCavalli(int num_cavalli, String name, int lentezza){
         super();
         this.num_cavalli = num_cavalli;
         this.name = name;
+        this.lentezza = lentezza;
     }
 
     @Override
     public void run(){
         setName(name);
-        System.out.println(Thread.currentThread().getName());
+        System.out.println("Cavallo " + name + " comincia la sua partenza!");
         for(int i=1; i<num_cavalli; i++){
             try{
-                sleep(200);
+                sleep(lentezza);
             }catch(InterruptedException e){
                 throw new RuntimeException();
             }
--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,13 @@ public class Main {
         System.out.println("Quanti giri vuoi fare? ");
         int count = scanner.nextInt();
 
-        CorsaCavalli thr1 = new CorsaCavalli(count, "Oca");
-        CorsaCavalli thr2 = new CorsaCavalli(count, "Tartuca");
-        CorsaCavalli thr3 = new CorsaCavalli(count, "Onda");
-        CorsaCavalli thr4 = new CorsaCavalli(count, "Aquila");
-        CorsaCavalli thr5 = new CorsaCavalli(count, "Istrice");
+        CorsaCavalli thr1 = new CorsaCavalli(count, "Oca", 100);
+        CorsaCavalli thr2 = new CorsaCavalli(count, "Tartuca", 150);
+        CorsaCavalli thr3 = new CorsaCavalli(count, "Onda", 90);
+        CorsaCavalli thr4 = new CorsaCavalli(count, "Aquila", 110);
+        CorsaCavalli thr5 = new CorsaCavalli(count, "Istrice", 123);
+
+
         thr1.start();
         thr2.start();
         thr3.start();
